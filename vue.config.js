afe9cfe7699579d59c6519f08b764c104bff72f8
KevinChiguano <kevin_chili1998@hotmail.com>
2024-07-20 16:44:40 -0500

    first commit

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+/*const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})*/
+
+
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7070',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
+});
+
+
